42258db4d57e3c50040607d32afb617818949832
Tristan Cormier <tristanc@openmailbox.org>
2017-12-27 00:30:47 -0500

    Updated the ConsoleStorm demo code

--- Source/ConsoleStorm.cpp
+++ Source/ConsoleStorm.cpp
@@ -1,20 +1,21 @@
-#include "H/STPL.h"
+#include <STPL.h>
 #include <cstdio>
-#include "H/SLock.h"
-#include "H/SThread.h"
 
 SMutex mutex;
+SMutex mutex2;
 
 void Test(const char *name)
 {
 	int i = 0;
-	mutex.Wait(INFINITE);
+	mutex2.Wait(INFINITE);
+	if (!strcmp(name, "thread1"))
+		i = -15;
 	do
 	{
 		i++;
 		printf("\tTest: %s\n", name);
 	} while (i < 5);
-	mutex.Release();
+	mutex2.Release();
 }
 
 unsigned long __stdcall DoWork1(void *)
@@ -36,7 +37,8 @@ int main(int argc, char *argv[])
 	SThread thread1(&DoWork1, 0);
 	SThread thread2(&DoWork2, 0);
 
-	mutex.Create(0, "MYMUTEX");
+	mutex.Create(0, "MYMUTEX"); // Testing creating mutexes
+	mutex2.Open("MYMUTEX"); // Testing opening mutexes
 
 	thread1.Join();
 	thread2.Join();
