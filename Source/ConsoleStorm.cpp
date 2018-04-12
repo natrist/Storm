3c8c8bfce8df075f3d49599f0aa8baff060d8ca3
Tristan Cormier <cormiert2@outlook.com>
2018-04-12 19:48:27 -0400

    Implemented SStrTokenize demo under ConsoleStorm.cpp

--- Source/ConsoleStorm.cpp
+++ Source/ConsoleStorm.cpp
@@ -1,4 +1,22 @@
 #include <STPL.h>
+
+int main(__in int _Argc, __in_ecount_z(_Argc) char ** _Argv, __in_z char ** _Env)
+{
+	char buf[255] = { 0, };
+	const char *string = "Zed 3 5 9";
+
+	SStrTokenize(&string, buf, -1, " \t");
+	printf("\tSStrTokenize returned '%s'\n", buf);
+	SStrTokenize(&string, buf, -1, " \t");
+	printf("\tSStrTokenize returned '%s'\n", buf);
+	SStrTokenize(&string, buf, -1, " \t");
+	printf("\tSStrTokenize returned '%s'\n", buf);
+	SStrTokenize(&string, buf, -1, " \t");
+	printf("\tSStrTokenize returned '%s'\n", buf);
+	return 0;
+}
+
+/*
 #include <cstdio>
 
 SMutex mutex;
@@ -46,3 +64,4 @@ int main(int argc, char *argv[])
 	mutex.Close();
 	return 0;
 }
+*/
